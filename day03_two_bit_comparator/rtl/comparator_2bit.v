c346d3047c92397394941bb318771e8fd7f28dd8
Vivek V <95570899+Vivek7349@users.noreply.github.com>
2026-02-08 22:01:49 +0530

    Revert "Day 03: To design and test 2 bit comparator"

--- day03_two_bit_comparator/rtl/comparator_2bit.v
+++
@@ -1,8 +0,0 @@
-module comparator_2bit (A, B, Greater, Lesser, Equal);
-	input A, B;
-	output Greater, Lesser, Equal;
-	
-	assign Greater = A & ~B;
-	assign Lesser = ~A & B;
-	assign Equal = ~(A ^ B);
-endmodule
